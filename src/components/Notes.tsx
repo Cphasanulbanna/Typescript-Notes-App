b178e37a4da39e30133ce2785cbfd5790177772d
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-05-18 21:10:32 +0530

    note card style completed

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,9 +1,23 @@
 import * as React from "react";
+import { Note } from "../models/note.model";
 
-interface INotesProps {}
+interface INotesProps {
+    note: Note;
+}
 
-const Notes: React.FC<INotesProps> = (props) => {
-    return <></>;
+const Notes: React.FC<INotesProps> = ({ note }) => {
+    return (
+        <div className="border-[1px] border-solid border-[gray] p-[24px] rounded-[4px] overflow-hidden w-[500px]">
+            <div className="note-card flex flex-col gap-[5px]">
+                <h1 className="text-[#111] text-[22px] font-bold">{note?.title}</h1>
+                <p className="text-grey text-[16px] font-semibold">{note?.text}</p>
+                <span className="text-[15px] text-[gray]">{note?.date}</span>
+                <button className="text-center w-[max-content] text-[16px] cursor-pointer rounded-[4px] overflow-hidden py-[10px] px-[20px] text-[#f1f1f1] bg-[red] ">
+                    Delete
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Notes;
--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -7,20 +7,24 @@ interface INotesListProps {
 }
 
 const NotesList: React.FC<INotesListProps> = ({ notes }) => {
-    console.log(notes);
-    const renderNotes = () => {
-        notes?.map((note) => {
-            <Notes
-                key={note.id}
-                note={note}
-            />;
+    const handleDelete = (id: string) => {};
+
+    const renderNotes = (): JSX.Element[] => {
+        return notes?.map((note) => {
+            return (
+                <Notes
+                    key={note.id}
+                    note={note}
+                    handleDelete={handleDelete}
+                />
+            );
         });
     };
     return (
-        <>
-            <h1 className="mt-[25px]">Notes</h1>
+        <div className="flex- flex-col p-[35px]">
+            <h1 className="mt-[25px] text-[#111] font-bold text-[26px]">Notes</h1>
             <div>{renderNotes()}</div>
-        </>
+        </div>
     );
 };
 
