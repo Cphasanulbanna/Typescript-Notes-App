a8be474e4f9d597193902831706fc3497f5618b7
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-05-18 20:26:16 +0530

    created type note

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
+import { Note } from "./models/note.model";
+
 type Props = {};
 
 const App = (props: Props) => {
+    const [notes, setNotes] = useState<Note[]>([
+        {
+            id: new Date().toString(),
+            title: "Typescript",
+            text: "Typescript is a superset of javascript",
+            color: "#dfdfdf",
+            date: new Date().toString(),
+        },
+    ]);
     return <div>App</div>;
 };
 
